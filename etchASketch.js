1d9b2868e30801945d7c526edc96269ff31b0a82
shkhkmt <93771404+shkhkmt@users.noreply.github.com>
2025-04-22 11:06:43 +0200

    completed Etch a Sketch. Come back to add opacity funcitonality

--- etchASketch.js
+++ etchASketch.js
@@ -12,48 +12,65 @@ document.body.appendChild(contentDiv);
 const header = document.createElement('header'); 
 header.id = 'header'; 
 header.textContent = 'Etch a Sketch'; 
+header.style.fontSize = '32px'; 
+header.style.fontWeight = '900'; 
 contentDiv.appendChild(header); 
 
 const btnPopup = document.createElement('button')
 btnPopup.id = 'btnPopup'; 
 btnPopup.textContent = 'change size'; 
-btnPopup.style.display = 'flex'; 
+btnPopup.style.display = 'flex';
+btnPopup.style.marginTop = '1em'; 
+btnPopup.style.fontSize = '16px'; 
 contentDiv.appendChild(btnPopup); 
 
 btnPopup.addEventListener('click', getNumSides) 
 let number; 
 
 function getNumSides() {
-  number = prompt("Choose number of squares per side: ");
-  createSketchDiv(number) 
-  return number; 
+  let input = prompt("Choose number of squares per side (max: 100): ");
+  
+  let number = parseInt(input);
+
+  if(isNaN(number) || number < 1 || number > 100) {
+    alert('Please choose a number between 1 and 100'); 
+  }
+    createSketchDiv(number);
 }
 
-function createSketchDiv(getNumSides) {
-  
-  if (document.getElementById('sketchDiv') !== null) {
-    const currentDiv = document.getElementById('sketchDiv'); 
-    contentDiv.removeChild(currentDiv)  
-  }
+const sketchDiv = document.createElement('div'); 
+sketchDiv.id = 'sketchDiv'; 
+sketchDiv.style.display = 'flex'; 
+sketchDiv.style.flexWrap = 'wrap';
+sketchDiv.style.flexDirection = 'column'; 
+sketchDiv.style.aspectRatio = '1 / 1';
+sketchDiv.style.width = `600px`; 
+contentDiv.appendChild(sketchDiv);
 
+function createSketchDiv(number) {
+  let crntDiv = document.getElementById('sketchDiv'); 
+  
+  if (crntDiv) {
+    contentDiv.removeChild(crntDiv); 
+  }
   const sketchDiv = document.createElement('div'); 
   sketchDiv.id = 'sketchDiv'; 
   sketchDiv.style.display = 'flex'; 
   sketchDiv.style.flexWrap = 'wrap';
+  sketchDiv.style.flexDirection = 'column'; 
   sketchDiv.style.aspectRatio = '1 / 1';
-  sketchDiv.style.width = `${20 *number}px`; 
-  contentDiv.appendChild(sketchDiv); 
-
+  sketchDiv.style.width = `600px`; 
+  contentDiv.appendChild(sketchDiv);
 
   for (let i = 1; i <= number * number; i++) {
-    const btnDiv = document.createElement('div'); 
-    btnDiv.id = `bDiv${i}`;
+    let btnDiv = document.createElement('div'); 
+    btnDiv.id = `btnDiv${i}`;
+    btnDiv.className = 'blocks'; 
     btnDiv.style.flex = '1 1 auto'; 
-    btnDiv.style.aspectRatio = '1 / 1'; 
-    btnDiv.style.width = '20px';  
-    btnDiv.style.height = '20px';
-    sketchDiv.appendChild(btnDiv);
-
+    btnDiv.style.aspectRatio = '1 / 1';
+    btnDiv.style.width = `${600/number}px`;  
+    btnDiv.style.height = `${600/number}px`; 
+    sketchDiv.appendChild(btnDiv); 
   } 
   sketchDiv.addEventListener('mouseover', bgChange); 
 }
@@ -68,8 +85,8 @@ function rndCol() {
 }
 
 function bgChange(event) {
-  let target = event.target; 
+  let target = event.target;
   let btn = document.getElementById(target.id); 
-  btn.style.backgroundColor = rndCol();  
+      btn.style.backgroundColor = rndCol();
 }
 
